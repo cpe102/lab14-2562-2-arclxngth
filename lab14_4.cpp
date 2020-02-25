4d0bacbb860c8e75252268dc2d64a590513c0616
arclxngth <59277471+arclxngth@users.noreply.github.com>
2020-02-25 16:25:23 +0700

    version 1

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-void shuffle(int &,int &, int &, int &);
+void shuffle(int &, int &, int &, int &);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
-	srand(time(0));	
+	srand(time(0));
 	
 	for(int i = 0;i < 10;i++){
 	    shuffle(a,b,c,d);
@@ -18,4 +19,21 @@ int main(){
 	return 0;
 }
 
-//Write definition of shuffle() here 
+void shuffle(int &a, int &b, int &c, int &d){
+
+	int arr[]={a,b,c,d};
+
+	for(int i=0;i<4;i++){
+
+		int x = rand()%4;
+
+		int temp = arr[i];
+		arr[i] = arr[x];
+		arr[x] = temp;
+
+	}
+	a = arr[0];
+	b = arr[1];
+	c = arr[2];
+	d = arr[3];
+}
--- lab14_5.cpp
+++ lab14_5.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *, int *, int *, int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
-	srand(time(0));	
+	srand(time(0));
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a, int *b, int *c, int *d){
+
+	int arr[]={*a,*b,*c,*d};
+
+	for(int i=0;i<4;i++){
+
+		int x = rand()%4;
+
+		int temp = arr[i];
+		arr[i] = arr[x];
+		arr[x] = temp;
+
+	}
+	*a = arr[0];
+	*b = arr[1];
+	*c = arr[2];
+	*d = arr[3];
+}
